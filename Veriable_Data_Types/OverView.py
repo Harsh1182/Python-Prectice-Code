83a24baa50e93436cc7c14a527767c6c8baaf837
Harsh1182 <harsh.g110802@gmail.com>
2025-01-09 16:03:39 +0530

    first commit

---
+++ Veriable_Data_Types/OverView.py
@@ -0,0 +1,16 @@
+# TODO: Define a variable called price1 and assign it the value 49.99
+price1 = 49.99
+# TODO: Define a variable called price2 and assign it the value 79.50
+price2 = 79.50
+# TODO: Define a variable called price3 and assign it the value 120.75
+price3 = 120.75
+
+# TODO: Calculate the total price and store the result in total_price
+total_price = (price1 + price2 + price3)
+# TODO: Calculate the average price and store the result in average_price
+average_price = (price1 + price2 + price3) / 3
+
+
+# TODO: Print the results for total and average prices
+print(f'The total price of the items is: {total_price}')
+print(f'The average price of the items is: {round(average_price,2)}')
---
+++ Veriable_Data_Types/Veriables.py
@@ -0,0 +1,3 @@
+# 1. Python is dynamicaly typed Programming Language.
+# 2. In Python while Declaraing Variables, we not need to metion data Types.
+# 3. 
